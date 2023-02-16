e2133b448115a7c6b1500c71022fbc5b7d3dfb42
NeethuJosephh <neethu1212001@gmail.com>
2023-02-17 02:08:32 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtformPageComponent } from './pages/artform-page/artform-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SigninPageComponent } from './pages/signin-page/signin-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SinglePageComponent } from './pages/single-page/single-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomePageComponent
+  },
+  {
+    path:'contact',component:ContactPageComponent
+  },
+  {
+    path:'signup',component:SignupPageComponent
+  },
+  {
+    path:'signin',component:SigninPageComponent
+  },
+  {
+    path:'artform',component:ArtformPageComponent
+  },
+  {
+    path:'single',component:SinglePageComponent
+  },
+ 
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ArtformPageComponent } from './pages/artform-page/artform-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { SigninPageComponent } from './pages/signin-page/signin-page.component';
+import { SinglePageComponent } from './pages/single-page/single-page.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { FooterComponent } from './units/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactPageComponent,
+    ArtformPageComponent,
+    SignupPageComponent,
+    SigninPageComponent,
+    SinglePageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
